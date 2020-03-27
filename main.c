4445271ece44f6190e56c64939d49d65655b19e5
locadm <43756894+locadm@users.noreply.github.com>
2020-03-27 19:31:09 +0100

    Input and mirror input

--- main.c
+++ main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    int i;
+    char input[501],mirror_input[501]={'\0'};
+
+    //READ STRINGS UNTIL NO NEW STRING IS ENTERED
+    while(gets(input)!=NULL){
+        if(input[0]=='\0')
+            return 0;
+
+
+
+        //CREATE A MIRROR COPY OF ORIGINAL
+        for(i=0;i<strlen(input);i++){
+            input[i]=toupper(input[i]);
+            mirror_input[i]=toupper(input[strlen(input)-1-i]);
+        }
+
+        printf("%s || %s\n",mirror_input, input);
+
+        for(i=0;i<=strlen(input);i++)
+        mirror_input[i]='\0';
+
+
+
+
+    }
 }
