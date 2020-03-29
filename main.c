2497268bfcf029468074eb1cf36db0e847b51dcd
locadm <43756894+locadm@users.noreply.github.com>
2020-03-29 13:03:22 +0200

    Working palindromes, WIP dynamic arrays

--- main.c
+++ main.c
@@ -2,11 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+char **palindrome_add(char **palindrome, char input_word[], int *palindrome_count){
+    int i;
 
+    palindrome = realloc(palindrome, (*(palindrome_count)+1) * sizeof(char*) );
+    palindrome[*palindrome_count] = malloc((strlen(input_word)+1)*sizeof(char));
+
+    for(i=0; i<=strlen(input_word); i++)
+        palindrome[*palindrome_count][i] = input_word[i];
+
+    (*palindrome_count)++;
+
+    return palindrome;
+}
 int main()
 {
-    int i;
-    char input[501],mirror_input[501]={'\0'};
+    int i,j,len_input,k,l,radius,palindrome_count=0;
+    char input[501],slovo[501]={'\0'},**palindrome = NULL;
 
     //READ STRINGS UNTIL NO NEW STRING IS ENTERED
     while(gets(input)!=NULL){
@@ -16,15 +28,38 @@ int main()
 
 
         //CREATE A MIRROR COPY OF ORIGINAL
-        for(i=0;i<strlen(input);i++){
-            input[i]=toupper(input[i]);
-            mirror_input[i]=toupper(input[strlen(input)-1-i]);
+        len_input=strlen(input);
+
+        radius=0;
+        for(i=1;i<len_input;i++){
+            while(toupper(input[i-radius-1])==toupper(input[i+radius])){
+                for(j=i-radius-1;j<=i+radius&&(i-radius>0&&i+radius<len_input);j++){
+                    printf("%c",input[j]);
+                }
+                radius++;
+                putchar('\n');
+            }
+            radius=1;
         }
 
-        printf("%s || %s\n",mirror_input, input);
+        //ODD
+        radius=1;
+        for(i=1;i<len_input;i++){
+            while(toupper(input[i-radius])==toupper(input[i+radius])){
+                for(j=i-radius;j<=i+radius&&(i-radius>=0&&i+radius<=len_input);j++){
+                    printf("%c",input[j]);
+                }
+                putchar('\n');
+                radius++;
 
-        for(i=0;i<=strlen(input);i++)
-        mirror_input[i]='\0';
+            }
+            radius=1;
+
+        }
+
+        putchar('\n');
+        //printf("%s || %s\n",mirror_input, input);
+
 
 
 
