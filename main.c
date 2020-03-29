23b38e2b594b041bcb33d2c847b549b7dcf8b559
locadm <43756894+locadm@users.noreply.github.com>
2020-03-29 13:58:48 +0200

    Complete

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ char **palindrome_add(char **palindrome, char input_word[], int *palindrome_coun
 }
 int main()
 {
-    int i,j,len_input,k,l,radius,palindrome_count=0;
+    int i,j,len_input,k,l,radius,palindrome_count=0,counter=0,longest=0;
     char input[501],slovo[501]={'\0'},**palindrome = NULL;
 
     //READ STRINGS UNTIL NO NEW STRING IS ENTERED
@@ -33,36 +33,71 @@ int main()
         radius=0;
         for(i=1;i<len_input;i++){
             while(toupper(input[i-radius-1])==toupper(input[i+radius])){
+                counter=0;
                 for(j=i-radius-1;j<=i+radius&&(i-radius>0&&i+radius<len_input);j++){
-                    printf("%c",input[j]);
+                    slovo[counter++]=input[j];
                 }
+                if(counter>longest){
+                    longest=counter;
+                }
+                slovo[counter]='\0';
+                if(strlen(slovo)>2)
+                    palindrome = palindrome_add(palindrome, slovo, &palindrome_count);
+
                 radius++;
-                putchar('\n');
             }
-            radius=1;
+            radius=0;
+
         }
 
         //ODD
         radius=1;
         for(i=1;i<len_input;i++){
             while(toupper(input[i-radius])==toupper(input[i+radius])){
+                counter=0;
                 for(j=i-radius;j<=i+radius&&(i-radius>=0&&i+radius<=len_input);j++){
-                    printf("%c",input[j]);
+                    slovo[counter++]=input[j];
+                    //printf("%c",input[j]);
                 }
-                putchar('\n');
+                if(counter>longest){
+                    longest=counter;
+                }
+                slovo[counter]='\0';
+                if(strlen(slovo)>2)
+                    palindrome = palindrome_add(palindrome, slovo, &palindrome_count);
+
                 radius++;
 
             }
             radius=1;
 
         }
+        if(longest){
+            printf("%d: ",longest);
+        }
+        else{
+            printf("No palindromes in this sentence.");
+        }
+        for(i=0;i<palindrome_count;i++){
+            if(strlen(palindrome[i])==longest)
+                printf("%s ",palindrome[i]);
+        }
+        printf("\n\n");
 
-        putchar('\n');
-        //printf("%s || %s\n",mirror_input, input);
+
+        for(i=0;i<palindrome_count;i++)
+            free(palindrome[i]);
+        longest=0;
+        palindrome_count=0;
+
+
 
 
 
 
 
     }
+    for(i=0;i<palindrome_count;i++){
+            free(palindrome[i]);
+        }
 }
